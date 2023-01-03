02f55daec16637641f66742f0594aee31c1fe2b5
Anthony Sogomo <tylorsoggy24@gmail.com>
2023-01-03 07:28:31 +0300

    Initial commit.

---
+++ arrays/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+int main()
+{
+    int luckyNumbers[] = {4, 8, 15, 16, 23, 42};
+    printf("%d", luckyNumbers[0]);
+
+    return(0);
+}
